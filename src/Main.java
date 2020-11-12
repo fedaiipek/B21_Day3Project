1f18e6990ade13c2de02540ccb0b0a04d1be48cc
fedaiipek <fedaiipek55@gmail.com>
2020-11-12 09:46:26 -0600

    tester

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         System.out.println("Hello Git Last Today");
         // B21 is excited!!!
         // Good job good money!!!
+        // B21 is sucsesfull tester
 
     }
 }
