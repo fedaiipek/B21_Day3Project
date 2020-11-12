d491d85c4f2502872debfce7e7abff80b47ba902
fedaiipek <fedaiipek55@gmail.com>
2020-11-12 10:22:05 -0600

    cocomelon3

--- src/Cocomelon.java
+++ src/Cocomelon.java
@@ -2,6 +2,7 @@ public class Cocomelon {
     public static void main(String[] args) {
         System.out.println("I will bet you Java");
         // cocomelon is not good for kids
+        // cocomelon is also good for dome people
 
     }
 }
