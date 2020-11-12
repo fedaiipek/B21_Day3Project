92160536e609247643b1d51db311e6fb8e77f8da
fedaiipek <fedaiipek55@gmail.com>
2020-11-12 10:32:45 -0600

    cocomelon4

--- src/Cocomelon.java
+++ src/Cocomelon.java
@@ -3,6 +3,7 @@ public class Cocomelon {
         System.out.println("I will bet you Java");
         // cocomelon is not good for kids
         // cocomelon is also good for dome people
+        // Class pet, jelly bean
 
     }
 }
